fb1fc4ac61c64b4373d21ced01ddd9d47c8cb2d6
s0ojin <100757599+s0ojin@users.noreply.github.com>
2022-11-03 23:45:51 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n').map(Number);
+
+const max = Math.max(...input);
+
+console.log(max);
+console.log(input.indexOf(max)+1);
