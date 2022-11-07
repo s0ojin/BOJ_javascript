e711d90e2970d0b69eb38c2628d7d4bf6a06dc35
s0ojin <100757599+s0ojin@users.noreply.github.com>
2022-11-08 00:35:15 +0900

    [Silver IV] Title: 한수, Time: 128 ms, Memory: 9784 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const N = +fs.readFileSync('/dev/stdin').toString();
+
+let isArithmetic = Array(N+1).fill(true);
+isArithmetic[0] = false;
+
+for(let num=1; num<=N; num++) {
+    const seperatedNumArr = String(num).split('').map(Number);
+    const d = seperatedNumArr[1]-seperatedNumArr[0];
+    
+    if(seperatedNumArr.length<3) {
+        continue;
+    } else {
+        for(let i=1; i<seperatedNumArr.length-1; i++){
+            if(seperatedNumArr[i+1]-seperatedNumArr[i] !== d) {
+                isArithmetic[num] = false;
+                break;
+            } 
+        }
+    }
+}
+console.log(isArithmetic.filter(boolean=> boolean === true).length);
