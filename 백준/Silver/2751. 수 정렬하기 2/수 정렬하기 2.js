15e9538100cacd42b156ec49a349b5b1788f9259
s0ojin <100757599+s0ojin@users.noreply.github.com>
2022-12-16 12:19:41 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2172 ms, Memory: 171900 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,11 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+input.shift();
+
+input.sort((a, b) => a - b);
+
+console.log(input.join("\n"));
