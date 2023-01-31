1a88772523aa6a4ed915e34a016721178b3dd5f8
s0ojin <100757599+s0ojin@users.noreply.github.com>
2023-01-31 18:31:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 132 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -0,0 +1,18 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim();
+
+const fomula = input.split("-");
+
+let answer = [];
+
+fomula.forEach((item) => {
+  if (item.includes("+")) {
+    let sum = 0;
+    item = item.split("+").map(Number);
+    item.forEach((i) => (sum += i));
+    answer.push(sum);
+  } else {
+    answer.push(Number(item));
+  }
+});
+
+console.log(answer.reduce((prev, cur) => prev - cur));
