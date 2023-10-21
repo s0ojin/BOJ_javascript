6edb8aae2d317ae53d7de8528d94ea8a5c831b0a
박수진 <100757599+s0ojin@users.noreply.github.com>
2023-10-21 20:39:09 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 136 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.js
@@ -0,0 +1,40 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("");
+
+const alphabetCouter = {
+  a: 0,
+  b: 0,
+  c: 0,
+  d: 0,
+  e: 0,
+  f: 0,
+  g: 0,
+  h: 0,
+  i: 0,
+  j: 0,
+  k: 0,
+  l: 0,
+  m: 0,
+  n: 0,
+  o: 0,
+  p: 0,
+  q: 0,
+  r: 0,
+  s: 0,
+  t: 0,
+  u: 0,
+  v: 0,
+  w: 0,
+  x: 0,
+  y: 0,
+  z: 0,
+};
+
+input.forEach((alphabet) => {
+  alphabetCouter[alphabet] += 1;
+});
+
+console.log(Object.values(alphabetCouter).join(" "));
