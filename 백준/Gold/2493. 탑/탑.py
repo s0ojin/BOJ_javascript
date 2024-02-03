05d32cd2d8a8f1ed22269d92bfe498f100b6f486
박수진 <100757599+s0ojin@users.noreply.github.com>
2024-02-03 23:26:36 +0900

    [Gold V] Title: 탑, Time: 740 ms, Memory: 154196 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.py
@@ -0,0 +1,15 @@
+N = int(input())
+towers = list(map(int, input().split()))
+answer = [0 for i in range(N)]
+stack = []
+
+for idx, tower in enumerate(towers):    
+    while stack and stack[-1][1] <= tower:
+        stack.pop()
+
+    if stack:
+        answer[idx] = stack[-1][0]
+
+    stack.append([idx+1, tower])
+
+print(' '.join(map(str,answer)))
