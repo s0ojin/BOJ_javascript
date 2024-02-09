e3272cc9522ab5470335c5c795ad0c863f5f5471
박수진 <100757599+s0ojin@users.noreply.github.com>
2024-02-09 14:23:24 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,30 @@
+import sys
+
+T = int(input())
+queue = []
+for i in range(T):
+    command = sys.stdin.readline().split()
+    if command[0] == 'push':
+        queue.append(command[1])
+    elif command[0] == 'pop':
+        if queue:
+            print(queue.pop(0))
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(queue))
+    elif command[0] == "empty":
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0) 
+    elif command[0] == "front":
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif command[0] == "back":
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
