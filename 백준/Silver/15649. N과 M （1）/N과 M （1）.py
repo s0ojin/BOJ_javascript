dcf2bbef815332ac66f783755f89f1fbd990b128
박수진 <100757599+s0ojin@users.noreply.github.com>
2024-05-02 23:25:13 +0900

    [Silver III] Title: N과 M (1), Time: 224 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,22 @@
+
+N, M = map(int, input().split())
+
+# 1부터 N까지 자연수에서 중복없이 M개 고른 수열
+
+
+stack = []
+
+
+def dfs(depth):
+    if depth == M:
+        print(*stack)
+
+    for i in range(1, N+1):
+        if i in stack:
+            continue
+        stack.append(i)
+        dfs(depth+1)
+        stack.pop()
+
+
+dfs(0)
