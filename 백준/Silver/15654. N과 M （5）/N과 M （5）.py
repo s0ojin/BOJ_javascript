14dd88c3a403e945d78d841200a36d52a2c7fdd9
박수진 <100757599+s0ojin@users.noreply.github.com>
2024-05-03 00:08:06 +0900

    [Silver III] Title: N과 M (5), Time: 192 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+stack = []
+visited = [0] * (N+1)
+
+nums.sort()
+
+
+def dfs(depth):
+    if depth == M:
+        print(*stack)
+        return
+
+    for idx, num in enumerate(nums):
+        if not visited[idx]:
+            visited[idx] = 1
+            stack.append(num)
+            dfs(depth+1)
+            stack.pop()
+            visited[idx] = 0
+
+
+dfs(0)
