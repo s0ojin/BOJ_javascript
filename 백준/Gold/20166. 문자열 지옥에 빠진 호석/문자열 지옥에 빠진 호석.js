4fb7dc77db25063dc01f3556294c426c7f59fd7d
박수진 <100757599+s0ojin@users.noreply.github.com>
2024-06-29 18:01:39 +0900

    [Gold IV] Title: 문자열 지옥에 빠진 호석, Time: 232 ms, Memory: 12712 KB, Score: 16 point -BaekjoonHub

--- 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.js
+++ 백준/Gold/20166. 문자열 지옥에 빠진 호석/문자열 지옥에 빠진 호석.js
@@ -7,7 +7,6 @@ const input = require("fs")
 const [N, M, K] = input.shift().split(" ").map(Number);
 const board = [];
 const answer = [];
-let cnt = 0;
 
 for (let i = 0; i < N; i++) {
   const row = input.shift().split("");
@@ -27,21 +26,17 @@ const dt = [
 
 const memo = {};
 
-const dfs = (x, y, depth, visited, str) => {
-  if (depth === str.length - 1) {
+const dfs = (x, y, depth, str) => {
+  if (depth === str.length) {
     return 1;
   }
-
-  const key = `${x}${y}${depth}`;
 
   let count = 0;
 
   for (let [dx, dy] of dt) {
     const [newX, newY] = [(x + dx + N) % N, (y + dy + M) % M];
-    if (!visited[newX][newY] && board[newX][newY] === str[depth + 1]) {
-      visited[newX][newY] = 1;
-      count += dfs(newX, newY, depth + 1, visited, str);
-      visited[newX][newY] = 0;
+    if (board[newX][newY] === str[depth]) {
+      count += dfs(newX, newY, depth + 1, str);
     }
   }
 
@@ -49,7 +44,7 @@ const dfs = (x, y, depth, visited, str) => {
 };
 
 for (const str of input) {
-  if (memo[str]) {
+  if (memo[str] !== undefined) {
     answer.push(memo[str]);
     continue;
   }
@@ -57,10 +52,8 @@ for (const str of input) {
   let cnt = 0;
   for (let i = 0; i < N; i++) {
     for (let j = 0; j < M; j++) {
-      // 문제의 첫글자와 일치하는 지점을 만나면 dfs 수행
       if (board[i][j] === str[0]) {
-        const visited = Array.from(Array(N), () => Array(M).fill(0));
-        cnt += dfs(i, j, 0, visited, str);
+        cnt += dfs(i, j, 1, str);
       }
     }
   }
