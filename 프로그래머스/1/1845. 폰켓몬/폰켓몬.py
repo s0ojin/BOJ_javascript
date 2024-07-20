1238e5ee184bba7dd57b234e2f845bf36cc8e3c4
박수진 <100757599+s0ojin@users.noreply.github.com>
2024-07-20 23:52:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.77 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,8 @@
+def solution(nums):
+    answer = 0
+    myList = set(nums)
+    if len(nums)/2 > len(myList):
+        answer = len(myList)
+    else:
+        answer = len(nums)/2
+    return answer
